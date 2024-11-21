b221589161c5b8e744a8353eef9ef97485954505
Diego Urrego <diegourrg@gmail.com>
2024-11-20 23:40:15 -0500

    feat: testing added to project cron-ticker and Dockerfile new comands learnt

--- cron-ticker/app.js
+++ cron-ticker/app.js
@@ -1,10 +1,6 @@
 const cron = require('node-cron');
-
-let times = 0;
-
-cron.schedule('1-59/5 * * * * *', () => {
-    times++;
-    console.log('Tick cada múltiplo de 5: ', times);
-});
+const {syncDB} = require("./tasks/sync-db");
 
 console.log('Inicio de la app')
+
+cron.schedule('1-59/5 * * * * *', syncDB);
